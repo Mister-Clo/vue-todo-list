308e4a308e56febe3c0e9eaf2f4dfc6404459246
Mister-Clo <49427147+Mister-Clo@users.noreply.github.com>
2022-04-04 18:39:48 +0200

    before bootstrap2

--- src/components/Exercice1.vue
+++ src/components/Exercice1.vue
@@ -5,18 +5,17 @@
     <form name="myform" action="#" class="box" id="new-command">
       <fieldset>
         <legend>Ajoutez une tâche</legend>
-
-        <input type="text" name="titre" placeholder="Titre" required />
-        <label for="completed">Effectué ? :</label><input type="checkbox" name="completed" id="check"><br><br>
-        <textarea rows="2" cols="40" name="contenu" placeholder="Donnez les détails de l'activité ici" required /><br>      
-
+        <input type="text" name="titre" placeholder="Titre" required /><br>
+        <textarea rows="2" cols="40" name="contenu" placeholder="Donnez les détails de l'activité ici" required /><br>
+        <label for="completed">Tâche déjà réalisé ? :</label><input type="checkbox" name="completed" id="check"><br>
+              
         <p>
           <button id="ajout" type="submit" class="btn">Ajouter</button>
         </p>
       </fieldset>
     </form>
 
-    <h3>Vos Tâches</h3>
+    <h3>Vos Tâches</h3><hr>
      <TodoList title="Le TP1" content="Finir le TP aujourd'hui" isCompleted/>
 
   
@@ -38,8 +37,16 @@ export default {
 </script>
 
 <style scoped>
+  form{
+    width:80%;
+    margin:auto;
+  }
   label{
     display: inline-block;
     margin-left: 2%;
+  }
+  input{
+    display: inline-block;
+    margin: 2%;
   }
 </style>
