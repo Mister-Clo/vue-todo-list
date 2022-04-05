f5683c741db377f3421ed1508e5106c5b6f879e3
Mister-Clo <49427147+Mister-Clo@users.noreply.github.com>
2022-04-05 10:35:33 +0200

    toggle completed tasks with method

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -12,8 +12,8 @@
     <tr v-for="item in items" :key="item">
       <td v-for="field in fields" :key="field">
 
-        <span v-if=" item[field]==true "><i @click="item['Etat'] = !item['Etat']" class="bi bi-check-all fs-2"></i></span>
-        <span v-else-if=" item[field]==false "><i @click="item['Etat'] = !item['Etat']" class="bi bi-x-square-fill fs-2"></i></span>
+        <span v-if=" item[field]==true "><i @click="toggleCheck(item)" class="bi bi-check-all fs-2"></i></span>
+        <span v-else-if=" item[field]==false "><i @click="toggleCheck(item)" class="bi bi-x-square-fill fs-2"></i></span>
         <span v-else>{{item[field]}}</span>
 
         </td>      
@@ -30,6 +30,12 @@ export default {
     items: {type: Array},
     fields:{type: Array}
   },
+
+  methods: {
+    toggleCheck : (item)=>{
+        item['Etat'] = !item['Etat']
+    }
+  }
  
 };
 </script>
